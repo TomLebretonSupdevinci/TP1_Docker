3edaa5d21f5947df64fe971dd689b9f357107d0b
Tom Lebreton <tom.lebreton@supdevinci-edu.fr>
2024-04-03 13:30:26 +0200

    RM fichier non utilisé

--- flask.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<h1>Bienvenue sur la page d'acceuil du TP Docker</h1> <p>Par Boulen Pierre et Lebreton Tom</p> <img src='https://c4.wallpaperflare.com/wallpaper/414/284/24/docker-containers-brand-wallpaper-preview.jpg'/>"
